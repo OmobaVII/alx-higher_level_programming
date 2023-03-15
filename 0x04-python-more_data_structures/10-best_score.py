270b4638be59b959c4ac40718b45caebfd306133
OmobaVII <omoba.career@gmail.com>
2023-03-15 09:49:44 -0700

    Best Score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,8 @@
 def best_score(a_dictionary):
     if (a_dictionary):
         new_dictionary = {}
-        new_dictionary.update(sorted(a_dictionary.items(), key=lambda a:a[1], reverse=True))
+        sort = sorted(a_dictionary.items(), key=lambda a: a[1], reverse=True)
+        new_dictionary.update(sort)
         for a, b in new_dictionary.items():
             return (a)
             break
