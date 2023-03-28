41c8241302753ff220dbb05add4a97cd90c2d2ed
OmobaVI <omoba.career@gmail.com>
2023-03-28 05:00:35 -0700

    Changes to meet code style

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -11,9 +11,17 @@ class Square:
     """
     def __init__(self, size=0):
         self.size = size
+
+        """
+        gives a private instance attribute for size
+        """
     @property
     def size(self):
         return self.__size
+
+    """
+    the property setter for the attribute
+    """
     @size.setter
     def size(self, value):
         if type(value) != int:
@@ -21,7 +29,9 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    """
+    returns the area of the square
+    """
     def area(self):
         return (self.__size ** 2)
-                
-
