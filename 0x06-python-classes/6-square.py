d11e16be7f0df88bb31c5ada2bcb62a9fc6a6e6a
OmobaVI <omoba.career@gmail.com>
2023-03-28 09:06:41 -0700

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,12 +1,29 @@
 #!/usr/bin/python3
+"""
+Module: 6-square
+defines a class square
+"""
+
+
 class Square:
+    """
+    definition of the class
+    """
+
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
 
+    """
+    gets the property size
+    """
     @property
     def size(self):
         return self.__size
+
+    """
+    sets the size attribute
+    """
     @size.setter
     def size(self, value):
         if type(value) != int:
@@ -15,16 +32,33 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    """
+    gets the property position
+    """
     @property
     def position(self):
         return self.__position
+
+    """
+    sets the position of the attribute
+    """
     @position.setter
     def position(self, value):
-        if type(value) != tuple or type(value[0]) != int or type(value[1]) != int:
+        if type(value) != tuple or type(value[0]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[1]) != int:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
+
+    """
+    returns the area of the square
+    """
     def area(self):
         return (self.__size ** 2)
+
+    """
+    prints the square using #
+    """
     def my_print(self):
         a = 0
         b = 0
