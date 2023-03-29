11cbd64c21e4b07987a55ec2be41cd64fdeef797
OmobaVI <omoba.career@gmail.com>
2023-03-29 04:23:57 -0700

    Print square instance

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,11 +1,34 @@
 #!/usr/bin/python3
+"""
+Module: 101-square.py
+define a class named Square
+"""
+
+
 class Square:
+    """
+    definition of the class
+    """
+
     def __init__(self, size=0, position=(0, 0)):
+        """
+        initializes the class
+        """
         self.size = size
         self.position = position
+
+    """
+    gets the property size
+    """
+
     @property
     def size(self):
         return self.__size
+
+    """
+    sets the property size
+    """
+
     @size.setter
     def size(self, value):
         if type(value) is not int:
@@ -13,9 +36,19 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    """
+    gets the property position
+    """
+
     @property
     def position(self):
         return self.__property
+
+    """
+    sets the property postion
+    """
+
     @position.setter
     def position(self, value):
         if type(value) is not tuple or len(value) != 2:
@@ -25,8 +58,18 @@ class Square:
         if type(value[1]) is not int or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integer")
         self.__position = value
+
+    """
+    returns the area of the square
+    """
+
     def area(self):
         return (self.__size ** 2)
+
+    """
+    print version of the sqaure
+    """
+
     def my_print(self):
         if self.__size == 0:
             print()
@@ -36,9 +79,15 @@ class Square:
             for i in range(self.__size):
                 for col in range(self.__position[0]):
                     print(" ", end="")
-                for j in range (self.__size):
+                for j in range(self.__size):
                     print("#", end="")
                 print()
+
+    """
+    give a string representation
+    to the square to return
+    """
+
     def __str__(self):
         a = 0
         b = 0
