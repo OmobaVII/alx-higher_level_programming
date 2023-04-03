c21552988d6501e34ca72ea02f1c187a6d3d2364
OmobaVI <omoba.career@gmail.com>
2023-04-03 05:00:39 -0700

    Simple Rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+this is the 0-rectangle module
+This module provides just one class the
+Rectangle class
+"""
+
+
+class Rectangle:
+    """
+    this is the rectangle class
+    it defines a rectangle
+    """
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+Learning About more classes in python
