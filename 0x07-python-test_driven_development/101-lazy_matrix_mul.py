74a64ebae991e3690cee80380a46771f0da801f7
OmobaVI <omoba.career@gmail.com>
2023-04-05 03:04:31 -0700

    Correction

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -5,37 +5,35 @@ this is the "101-lazy_matrix_mul" module
 this module provide one function, lazy_matrix_mul()
 this function takes in two matrix as parameter and returns thier product
 """
+
+
 def lazy_matrix_mul(m_a, m_b):
     if type(m_a) is not list:
-        raise TypeError("m_a must be a list")
+        raise TypeError("Scalar operands are not allowed, use '*' instead")
     if type(m_b) is not list:
-        raise TypeError("m_b must be a list")
+        raise TypeError("Scalar operands are not allowed, use '*' instead")
     for lists_a in m_a:
         if type(lists_a) is not list:
             raise TypeError("m_a must be a list of lists")
     for lists_b in m_b:
         if type(lists_b) is not list:
             raise TypeError("m_b must be a list of lists")
-    if len(m_a) == 0:
-        raise ValueError("m_a can't be empty")
-    if len(m_b) == 0:
-        raise ValueError("m_b can't be empty")
     for list_a in m_a:
         for i in list_a:
             if type(i) not in [int, float]:
-                raise TypeError("m_a should contain only integers or floats")
+                raise TypeError("invalid data type for einsum")
     for list_b in m_b:
         for j in list_b:
             if type(j) not in [int, float]:
-                raise TypeError("m_b should contain only integers or floats")
+                raise TypeError("invalid data type for einsum")
     col_a = len(m_a[0])
     for items in m_a:
         if len(items) != col_a:
-            raise TypeError("each row of m_a must be of the same size")
+            raise TypeError("setting an array element with a sequence.")
     col_b = len(m_b[0])
     for item in m_b:
         if len(item) != col_b:
-            raise TypeError("each row of m_b must be of the same size")
+            raise TypeError("setting an array element with a sequence.")
     row_a = len(m_a)
     row_b = len(m_b)
     if row_a != col_b and row_b != col_a:
