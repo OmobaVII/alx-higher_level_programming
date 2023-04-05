6403bc1aaca9e42a5a9ef550575e047d6f0956cf
OmobaVI <omoba.career@gmail.com>
2023-04-05 04:14:29 -0700

    Correction

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -46,11 +46,10 @@ def lazy_matrix_mul(m_a, m_b):
             raise TypeError("setting an array element with a sequence.")
     row_a = len(m_a)
     row_b = len(m_b)
+    msg = ("shapes ({:d},{:d}) and ({:d},{:d}) not aligned: {:d} (dim {:d}) != {:d} (dim {:d})"
+            .format(col_b, row_a, col_b, col_b, row_a, row_b, col_b, row_a))
     if row_a != col_b and row_b != col_a:
-        raise ValueError("shapes ({:d},{:d}) and ({:d},{:d})\
-                         not aligned: {:d} (dim {:d}) != {:d} (dim {:d})"\
-                         .format(col_b, row_a, col_b, col_b, row_a, row_b,
-                         col_b, row_a))
+        raise ValueError(msg)
     new_matrix = [[]]
-    new_matrix = np.dot(m_a, m_b)
+    new_matrix = np.matmul(m_a, m_b)
     return new_matrix
