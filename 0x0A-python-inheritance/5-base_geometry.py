f4574d4895b20836072e8fc5199bb355626e1221
OmobaVI <omoba.career@gmail.com>
2023-04-11 07:19:05 -0700

    Geometry Module

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+this is the `5-base_geometry' module
+This module has just one class
+BaseGeometry()
+"""
+
+
+class BaseGeometry:
+    """defines a class called BaseGeometry"""
