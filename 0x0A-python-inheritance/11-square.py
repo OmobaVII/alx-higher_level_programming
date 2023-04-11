31d85c43e1497a002a49ce13190fb68638b8a5af
OmobaVI <omoba.career@gmail.com>
2023-04-11 12:11:39 -0700

    Square #2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+this is the '11-square' module
+This module defines a class square
+Square is a subclass of Rectangle
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+"""imports the Rectangle class"""
+
+
+class Square(Rectangle):
+    """defines the class Square which is a sub class of rectangle"""
+    def __init__(self, size):
+        """initializes the rectangle with size"""
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        s = ""
+        s += "[Square] {:d}/{:d}".format(self.__size, self.__size)
+        return s
