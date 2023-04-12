18f76e3083857d32544a4fe28ac1bdce083de647
OmobaVI <omoba.career@gmail.com>
2023-04-12 10:21:09 -0700

    Class to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -6,12 +6,8 @@ class_to_json()
 """
 
 
-import json
-"""module important for json to run"""
-
-
 def class_to_json(obj):
     """returns the dictionary description"""
 
-    s = json.dumps(obj, default=lambda x: x.__dict__)
+    s = obj.__dict__
     return s
