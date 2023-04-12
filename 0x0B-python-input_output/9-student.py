a1f36703a10f6dddff7749491f4117495a502ba1
OmobaVI <omoba.career@gmail.com>
2023-04-12 10:47:05 -0700

    Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+the 9-student module
+this module defines a class
+Student()
+"""
+
+
+class Student:
+    """defines the class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation"""
+        return self.__dict__
