8f8f480b30d205f890882434fe09c0ef56be1186
OmobaVI <omoba.career@gmail.com>
2023-04-13 07:17:20 -0700

    Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -8,17 +8,13 @@ this module is a script
 import sys
 """important for the stdin"""
 
-from collections import defaultdict
-"""for the dict"""
-
 def print_the_stats(size, stat_codes):
-    print(f"File size: {size}")
+    print("File size: {}".format(size))
     for key in sorted(stat_codes):
-        print(f"{key}: {stat_codes[key]}")
+        print("{}: {}".format(key, stat_codes[key]))
 
 if __name__ == "__main__":
     
-
     size = 0
     stat_codes = dict()
     valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
@@ -35,15 +31,21 @@ if __name__ == "__main__":
             line = line.split()
 
             try:
+                size += int(line[-1])
+            except (ValueError, IndexError):
+                pass
+
+            try:
                 if line[-2] in valid_codes:
                     if stat_codes.get(line[-2], -1) == -1:
                         stat_codes[line[-2]] = 1
                     else:
                         stat_codes[line[-2]] += 1
-            except IndexError:
+            except (IndexError, ValueError):
                 pass
+
         print_the_stats(size, stat_codes)
 
     except KeyboardInterrupt:
         print_the_stats(size, stat_codes)
-
+        raise
