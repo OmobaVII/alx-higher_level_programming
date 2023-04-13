05bb80dd49bf771972e0a66352ac5802ea4f61a1
OmobaVI <omoba.career@gmail.com>
2023-04-13 02:59:54 -0700

    Search and Update

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+this is the `100-append_after` module
+this module provides one function
+append_after()
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    with open(filename, "r") as myFile:
+        lines = myFile.readlines()
+
+    with open(filename, "w") as myFile:
+        for line in lines:
+            myFile.write(line)
+            if search_string in line:
+                myFile.write(new_string)
