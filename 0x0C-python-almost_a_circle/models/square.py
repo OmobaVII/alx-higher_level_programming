a62c78dc56ee417dcae86046c62e546e7e282c56
OmobaVI <omoba.career@gmail.com>
2023-04-15 08:25:58 -0700

    Square update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -19,7 +19,7 @@ class Square(Rectangle):
     def __str__(self):
         """returns a string representation in a format"""
         s = "[Square] ({:d}) {:d}/{:d} - {:d}"
-        r = s.format(self.id, self.x, self.y, self.size)
+        r = s.format(self.id, self.x, self.y, self.width)
         return r
 
     @property
@@ -32,3 +32,17 @@ class Square(Rectangle):
         """sets the value of size"""
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
