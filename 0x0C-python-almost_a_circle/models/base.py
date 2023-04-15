971f1ba9de0e95dc54b07a0ae8323e25d32421b0
OmobaVI <omoba.career@gmail.com>
2023-04-15 13:29:08 -0700

    JSON ok but CSV?

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -10,6 +10,10 @@ import json
 """needed for the JSON implementations"""
 
 
+import csv
+"""needed for the csv files"""
+
+
 class Base:
     """the definition of the class"""
 
@@ -75,3 +79,40 @@ class Base:
 
         object_list = cls.from_json_string(string)
         return [cls.create(**obj) for obj in object_list]
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """serialize to CSV"""
+        filename = cls.__name__+".csv"
+        if list_objs is None:
+            list_objs = []
+        with open(filename, "w", newline="") as myFile:
+            for obj in list_objs:
+                if cls.__name__ == "Rectangle":
+                    csv.writer(myFile).writerow([obj.id, obj.width, obj.height, obj.x, obj.y])
+                elif cls.__name__ == "Square":
+                    csv.writer(myFile).writerow([obj.id, obj.size, obj.x, obj.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """deserialize from csv"""
+        instances = []
+        filename = cls.__name__ + ".csv"
+
+        with open(filename, "r", newline="") as myFile:
+            reader = csv.reader(myFile)
+            for row in reader:
+                if cls.__name__ == "Rectangle":
+                    instance = {"id": int(row[0]),
+                                "width": int(row[1]),
+                                "height": int(row[2]),
+                                "x": int(row[3]),
+                                "y": int(row[4])}
+                elif cls.__name__ == "Square":
+                    instance = {"id": int(row[0]),
+                                "size": int(row[1]),
+                                "x": int(row[2]),
+                                "y": int(row[3])}
+                diction = cls.create(**instance)
+                instances.append(diction)
+        return instances
