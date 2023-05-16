eb90428cf6fe198a6e1d9e851c1458a1dc4dbb1c
OmobaVII <omoba.career@gmail.com>
2023-05-16 14:00:15 -0700

    Temperatures

--- 0x0D-SQL_introduction/test.sql
+++ 0x0D-SQL_introduction/test.sql
@@ -1,4 +1,3 @@
--- Creates a table second_table in the database hbtn_0c_0
+-- lists all rows of a table first_table
 
-INSERT INTO second_table (id, name, score) VALUES (5, "Aria", 18);
-INSERT INTO second_table (id, name, score) VALUES (6, "Aria", 12);
+SELECT * FROM temperatures;
