6b821ee0fecbcbb4f7c76016772de12ba33354f4
OmobaVII <omoba.career@gmail.com>
2023-05-16 13:33:07 -0700

    Go to UTF8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,9 @@
+-- Converts the table hbtn_0 to UTF8(utf8mb4, collate utf8mb4_unicode_ci)
+
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
