eea363563c2e5d71c45bb81d9815ea7bff037e8d
OmobaVII <omoba.career@gmail.com>
2023-05-17 07:08:42 -0700

    Cities by States

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- Lists all cites in hbtn_0d_usa
 
-SELECT id, cities.name, states.name
+SELECT cities.id, cities.name, states.name
 FROM cities
 NATURAL JOIN states
 WHERE cities.state_id = cities.id
