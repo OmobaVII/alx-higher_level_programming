5e1cee3b4d5ca8955098a327d3e73e299245cc80
OmobaVII <omoba.career@gmail.com>
2023-05-17 10:11:52 -0700

    No genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- Lists all shows in hbtn_od_tvshows without a genre linked
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id is NULL
+ORDER BY title, tv_show_genres.genre_id;
