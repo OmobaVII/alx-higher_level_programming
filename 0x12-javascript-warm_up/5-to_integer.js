64815a3ef7ca84f92d92c3bd5263b0f38e09bfa9
OmobaVII <omoba.career@gmail.com>
2023-06-13 13:17:19 -0700

    An integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Prints My number: first argument converted in integer
+
+let { argv } = require('process');
+if (isNaN(parseInt(argv[2]))) {
+  console.log('Not a number');
+} else {
+  console.log('My number:', parseInt(argv[2]));
+}
