d1ceaec1d227c602a6e110914e03da1a5c82a763
OmobaVII <omoba.career@gmail.com>
2023-06-13 16:29:20 -0700

    Add me maybe

---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// A function that increments and calls a function
+
+function addMeMaybe (number, theFunction) {
+  theFunction(number + 1);
+}
+module.exports = {
+  addMeMaybe
+};
