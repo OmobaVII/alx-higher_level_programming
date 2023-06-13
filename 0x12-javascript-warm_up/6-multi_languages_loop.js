0ed34175f8478e8e88f3cb86b96dacbcf3252b93
OmobaVII <omoba.career@gmail.com>
2023-06-13 13:31:39 -0700

    Loop to languages:

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Prints 3 lines using a loop
+
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let num = 0;
+while (num < 3) {
+  console.log(lines[num]);
+  num++;
+}
