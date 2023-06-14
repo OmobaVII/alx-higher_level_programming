48e27718de5098b823b40d6b36ea0e59de8d28db
OmobaVII <omoba.career@gmail.com>
2023-06-14 14:34:33 -0700

    Log me

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// a function that prints the number of arguments already printes and the new argument
+
+let num = 0;
+exports.logMe = function (item) {
+  console.log(num + ": " + item);
+  num++;
+}
