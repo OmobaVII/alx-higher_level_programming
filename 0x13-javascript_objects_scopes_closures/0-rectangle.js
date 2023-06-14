48e9dcbf30d60f4d60a00cf3857652693a02ce9a
OmobaVII <omoba.career@gmail.com>
2023-06-14 12:01:26 -0700

    Rectangle #0

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Defines and empty class Rectangle
 
-class Rectangle {
-    
-};
+class Rectangle {}
+// module.exports = class Rectangle {};
+// defines the empty class and exports it in a single line
 module.exports = Rectangle;
