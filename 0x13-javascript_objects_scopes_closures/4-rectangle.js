b54fbf5820e3d69e9ad42ae194c09441755bc308
OmobaVII <omoba.career@gmail.com>
2023-06-14 12:38:33 -0700

    Rectangle #4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+// Create an instance methods rotate and double on the Rectangle class
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let num = 0;
+    while (num < this.height) {
+      console.log('X'.repeat(this.width));
+      num++;
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
