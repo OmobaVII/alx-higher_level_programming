1fb0726e6fb45bb9caf403dc0c7465504dc32bf4
OmobaVII <omoba.career@gmail.com>
2023-06-17 00:22:33 -0700

    Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+A script that lists all states from the
+database hbtn_0e_0_usa
+The script takes 3 arguments
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """all code insdie this block wont be
+    excuted if imported"""
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         database=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id")
+    for state in cur.fetchall():
+        if state[1][0] == "N":
+            print(state)
+    cur.close()
+    db.close()
