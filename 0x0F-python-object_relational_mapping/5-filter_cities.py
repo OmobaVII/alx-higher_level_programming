41a3fc2bf9da1a67a5a2ee432b44c1d9cc5c5a2f
OmobaVII <omoba.career@gmail.com>
2023-06-17 01:55:16 -0700

    All cities by state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+A script that lists all cities
+from the database hbtn_0e_4_usa
+Sorted in ascending by cities id
+"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    """all code inside this block wont be
+    executed if imported"""
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         database=argv[3])
+    cur = db.cursor()
+    query = """SELECT cities.name
+               FROM states
+               JOIN cities
+               ON states.id = cities.state_id
+               WHERE states.name LIKE %s
+               ORDER BY cities.id"""
+
+    cur.execute(query, (argv[4], ))
+    for city in cur.fetchall():
+        print(city[0], end=", ")
+
+    print()
+    cur.close()
+    db.close()
