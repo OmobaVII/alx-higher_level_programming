41298d1859750fd68a4f999e682d66cd7831d681
OmobaVII <omoba.career@gmail.com>
2023-06-17 04:17:28 -0700

    Added the if name so when imported does not run

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,12 +9,14 @@ from sqlalchemy import create_engine
 from model_state import Base, State
 from sys import argv
 
+if __name__ == "__main__":
+    """so if imported does not execute"""
 
-engine = create_engine("mysql://{}:{}@localhost:3306/{}".
-                       format(argv[1], argv[2], argv[3]))
-Session = sessionmaker(bind=engine)
-session = Session()
+    engine = create_engine("mysql://{}:{}@localhost:3306/{}".
+                           format(argv[1], argv[2], argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-for instance in session.query(State).order_by(State.id):
-    print("{:d}: {:s}".format(instance.id, instance.name))
-session.close()
+    for instance in session.query(State).order_by(State.id):
+        print("{:d}: {:s}".format(instance.id, instance.name))
+    session.close()
