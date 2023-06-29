6839930a3bc50385345d1e1b8bd8dd531089216d
OmobaVII <omoba.career@gmail.com>
2023-06-29 15:51:01 -0700

    Catch me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #i/bin/bash
 # Makes a request to a server that cuases the server to respond with a message
-curl -sL -X PUT -d "user_id=98" -H "Origin: School" -L 0.0.0.0:5000/catch_me_2
+curl -sL "0.0.0.0:5000/catch_me" -X PUT -H "Origin: School" -d "user_id=98"
