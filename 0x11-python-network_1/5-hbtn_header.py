d42c039245a336ca98173aaae7ea07e96cfc3761
OmobaVII <Omoba.career@gmail.com>
2023-06-30 09:30:21 -0700

    Response header value #1

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module takes in a URL,
+Sends a request to the URL
+Displays the Value of the X-Request-Id Variable
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """Makes the code not to be executed when imported"""
+    response = requests.get(argv[1])
+    value = response.headers["X-Request-Id"]
+    print(value)
