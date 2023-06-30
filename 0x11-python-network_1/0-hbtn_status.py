74a1302066f5539027df892eba5be5ab7c527178
OmobaVII <Omoba.career@gmail.com>
2023-06-30 05:45:34 -0700

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,15 +2,15 @@
 """
 This module fetches `https://alx-intranet.hbtn.io/status`
 """
-import urllib.request
+from urllib.request import urlopen
 
-
-with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-    html = response.read()
-
-print('Body response:')
-print('    - type:', type(html))
-print('    - content:', html)
-print('    - utf8 content:', html.decode('utf-8'))
 
 if __name__ == "__main__":
+    """Prevents from being executed"""
+    with urlopen("https://alx-intranet.hbtn.io/status") as response:
+        html = response.read()
+
+    print('Body response:')
+    print('    - type:', type(html))
+    print('    - content:', html)
+    print('    - utf8 content:', html.decode('utf-8'))
