6a484d0f8aed8702e1a383622cded91aae35a9a6
OmobaVII <Omoba.career@gmail.com>
2023-07-01 03:30:23 -0700

    Time for an interview! An ALX Interview

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Takes 2 arguments
+1 respository name
+2 owner name
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """codes wont run if imported"""
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    response = requests.get(url)
+    response = response.json()
+
+    for commits in response[0:10]:
+        print(commits.get("sha"), end=":")
+        print(commits.get("commit").get("author").get("name"))
