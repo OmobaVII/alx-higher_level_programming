61e98281011e027989631dfac8ac83329b5a21a9
OmobaVII <omoba.career@gmail.com>
2023-07-25 05:39:21 -0700

    Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Displays the status code of a GET request
+
+const { argv } = require('process');
+const request = require('request');
+
+request.get(argv[2], function (err, response) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('code: ', response.statusCode);
+  }
+});
