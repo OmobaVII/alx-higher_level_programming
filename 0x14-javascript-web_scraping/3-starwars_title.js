4cfa399cb31ce6d9425fe67ca11d9b325edd0b78
OmobaVII <omoba.career@gmail.com>
2023-07-25 06:11:39 -0700

    Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Prints the title of a Star Wars movie where the episode number matches a given integer
+
+const { argv } = require('process');
+const request = require('request');
+url = 'https://swapi-api.alx-tools.com/api/films/' + argv[2]
+request(url, function (err, response, body) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
