05b3e976b322fc27c1bb47e57206b7ef87a109c1
OmobaVII <omoba.career@gmail.com>
2023-07-25 08:14:19 -0700

    How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -9,13 +9,14 @@ request(argv[2], function (err, response, body) {
     console.error(err);
   } else if (response.statusCode === 200) {
     const output = {};
-    const data = JSON.parse(body);
-    for (const a in data) {
-      if (data[a].completed) {
-        if (output[data[a].userId] === undefined) {
-          output[data[a].userId] = 1;
+    const result = JSON.parse(body);
+    for (const a in result) {
+      const data = result[a]
+      if (data.completed) {
+        if (output[data.userId] === undefined) {
+          output[data.userId] = 1;
         } else {
-          output[data[a].userId] += 1;
+          output[data.userId]++;
         }
       }
     }
