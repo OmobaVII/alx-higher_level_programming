e92ce96a0624dae132209a259b0eaa64c32c95ea
OmobaVII <omoba.career@gmail.com>
2023-07-25 07:02:51 -0700

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,18 +4,17 @@
 const { argv } = require('process');
 const request = require('request');
 
-let number = 0;
-
 request(argv[2], function (err, response, body) {
   if (err) {
     console.error(err);
   } else {
     const data = JSON.parse(body).results;
+    let number = 0;
     for (const movie of data) {
-      if (movie.characters.includes("https://swapi-api.alx-tools.com/api/people/18/")) {
+      if (movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
         number += 1;
-      };
+      }
     }
     console.log(number);
-  };
+  }
 });
