8bd9ebae1a997db26e8b0d34e59a95c815080037
OmobaVII <omoba.career@gmail.com>
2023-08-02 06:44:40 -0700

    TOggle classes

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,6 @@
+// Toggles the class of the <header> element whenthe user clicks on the tag toggle_header
+$('#toggle_header').click(
+  function () {
+    $('header').toggleClass('green red');
+  }
+);
