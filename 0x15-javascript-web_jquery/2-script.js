18237ba30bdb58fac7ae41425ff4587a097fce17
OmobaVII <omoba.career@gmail.com>
2023-08-02 06:10:11 -0700

    Click and turn red

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,6 @@
+// Updates the text color of the header element to ##FF0000 when the user clicks on the tag rDIV#red_header
+$('#red_header').click(
+  function () {
+    $('header').css('color', '#FF0000');
+  }
+);
