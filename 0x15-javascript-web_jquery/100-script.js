1924876a47cd5ce4768c303727f0940ba09fd41e
OmobaVII <omoba.career@gmail.com>
2023-08-02 13:20:55 -0700

    No jQuery

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+// updates the text color of the header element to #FF0000 script is imported from the head
+document.addEventListener('DOMContentLoaded', function () {
+  const header = document.querySelector('header');
+  header.style.color = '#FF0000';
+});
